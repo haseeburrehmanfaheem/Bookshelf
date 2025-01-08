7b1aee7c0f79067fe66ce0fb5e27c3d62e15cdf7
haseeb62 <23100044@lums.edu.pk>
2025-01-08 23:40:17 +0500

    changes made

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Use the environment variable, or fall back to localhost for dev
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+// const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const baseURL = "/";
 
 const api = axios.create({
   baseURL: baseURL,
